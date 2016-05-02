500cb12f9bf44eeb914c6b64a09cc11874aa6af5
kg <kjg310@gmail.com>
2016-05-02 18:06:53 -0400

    Everything done except for the matching

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,19 @@
 var friends = [
 
 {
-	name: 'Alex',
-	scores: [2, 3]
+	name: 'Billy',
+	image: 'http://www.peta.org/wp-content/uploads/2016/02/Goats-maximili-602x403.jpg',
+	scores: [2, 3, 4, 5, 3, 2, 1, 2, 3, 5]
 },
 {
-	name: 'Ryan',
-	scores: [1, 1]
+	name: 'Donald',
+	image: 'http://c1.nrostatic.com/sites/default/files/uploaded/donald-trump-incapable-of-embarrassment-r_0.jpg',
+	scores: [1, 1, 2, 3, 1, 4, 5, 2, 3, 1]
 },
 {
-	name: 'Jake',
-	scores: [4, 5]
+	name: 'Bernie',
+	image: 'https://01.img.society6.com/society6/img/nQs0Oq4hVzuNFynfZJQMX-7v9Bs/w_700/artwork/~artwork/s6-0066/a/27326061_9971749/~~/bernie-and-kittens-prints.jpg',
+	scores: [4, 5, 4, 2, 3, 4, 3, 5, 3, 4]
 }
 
 ];
